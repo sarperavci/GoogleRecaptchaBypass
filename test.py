6c2edb53a11799aab8557889ce18a42e9a805b8d
Sarper AVCI <50243344+sarperavci@users.noreply.github.com>
2025-07-15 20:28:48 +0300

    Add stealth args for demo

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
-from DrissionPage import ChromiumPage 
+from DrissionPage import ChromiumPage, ChromiumOptions
 from RecaptchaSolver import RecaptchaSolver
 import time
 
+CHROME_ARGUMENTS = [
+    "-no-first-run",
+    "-force-color-profile=srgb",
+    "-metrics-recording-only",
+    "-password-store=basic",
+    "-use-mock-keychain",
+    "-export-tagged-pdf",
+    "-no-default-browser-check",
+    "-disable-background-mode",
+    "-enable-features=NetworkService,NetworkServiceInProcess",
+    "-disable-features=FlashDeprecationWarning",
+    "-deny-permission-prompts",
+    "-disable-gpu",
+    "-accept-lang=en-US",
+    "--disable-usage-stats",
+    "--disable-crash-reporter",
+    "--no-sandbox"
+]
  
-driver = ChromiumPage()
+options = ChromiumOptions()
+for argument in CHROME_ARGUMENTS:
+    options.set_argument(argument)
+    
+driver = ChromiumPage(addr_or_opts=options)
 recaptchaSolver = RecaptchaSolver(driver)
 
 driver.get("https://www.google.com/recaptcha/api2/demo")
